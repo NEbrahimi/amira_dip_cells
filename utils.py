f6b3f3713b3ebe575d3db47ee077621cd1c39e09
NEbrahimi <nebr002@aucklanduni.ac.nz>
2018-07-12 18:39:02 +1200

    Some modifications and new functions.

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import numpy as np
-import os
-
-root_dir = 'C:/Users/nebr002/Desktop/53_WF/CSV/'
 
 
-def get_centroid(filename, delim=',', header=True, cols=None):
+def get_data(filename, delim=',', header=True, cols=None):
     import csv
 
     with open(filename, 'r') as f:
@@ -51,10 +48,15 @@ def translate(c1, xy_interp):
     return c1_new
 
 
-def save_file(filename_original, translated_values, header=True, cols=None):
+def save_file(filename_original, values, option='translation', header=True, cols=None):
     import csv
+    eigenrow = ['EVector1x','EVector1y','EVector1z','EVector2x','EVector2y','EVector2z',
+                'EVector3x','EVector3y','EVector3z','EValue1','EValue2','EValue3']
 
-    new_file = filename_original.replace('.csv', '_translated.csv')
+    if option == 'translation':
+        new_file = filename_original.replace('.csv', '_translated.csv')
+    elif option == 'eigen':
+        new_file = filename_original.replace('.csv', '_eigen.csv')
 
     with open(filename_original, 'r') as f:
         print("Saving the new file. Please wait...")
@@ -63,25 +65,59 @@ def save_file(filename_original, translated_values, header=True, cols=None):
         with open(new_file, 'w') as w:
             writer = csv.writer(w, delimiter=',', quotechar='|', lineterminator='\n')
 
-            if header:
+            if option == 'eigen':
+                header_row = []
+                temp_header = reader.next()
+                for i in range(len(temp_header)):
+                    header_row.append(temp_header[i])
+                header_row.extend(eigenrow)
+                writer.writerow(header_row)
+            elif option == 'translation':
                 writer.writerow(reader.next())
+            else:
+                raise Exception("Incorrect header option!")
+
 
         with open(new_file, 'a') as a:
             writer = csv.writer(a, delimiter=',', quotechar='|', lineterminator='\n')
 
             count = 0
-            for row in reader:
-                if cols is None:
-                    writer.writerow(row)
-                else:
-                    new_row = row[0:cols[0]]
-                    new_row.append(translated_values[:,0][count])
-                    new_row.append(translated_values[:,1][count])
-                    new_row.append(translated_values[:,2][count])
-                    new_row.extend(row[cols[1]:])
 
+            for row in reader:
+                if cols is None and option == 'translation':
+                    new_row = []
+                    new_row.append(values[:,0][count])
+                    new_row.append(values[:,1][count])
+                    new_row.append(values[:,2][count])
                     writer.writerow(new_row)
-                    count+=1
+                elif cols is not None and option == 'translation':
+                    new_row = []
+                    new_row.extend(row[0:cols[0]])
+                    new_row.append(values[:,0][count])
+                    new_row.append(values[:,1][count])
+                    new_row.append(values[:,2][count])
+                    new_row.extend(row[cols[1]:])
+                    writer.writerow(new_row)
+                elif cols is None and option == 'eigen':
+                    new_row = []
+                    new_row.extend(row)
+                    new_row.append(values[:,0][count])
+                    new_row.append(values[:,1][count])
+                    new_row.append(values[:,2][count])
+                    new_row.append(values[:,3][count])
+                    new_row.append(values[:,4][count])
+                    new_row.append(values[:,5][count])
+                    new_row.append(values[:,6][count])
+                    new_row.append(values[:,7][count])
+                    new_row.append(values[:,8][count])
+                    new_row.append(values[:,9][count])
+                    new_row.append(values[:,10][count])
+                    new_row.append(values[:,11][count])
+                    writer.writerow(new_row)
+                else:
+                    raise Exception("Incorrect Options or Cols!")
+
+                count+=1
 
         print("count is %i" %count)
         f.close()
@@ -90,19 +126,31 @@ def save_file(filename_original, translated_values, header=True, cols=None):
     return None
 
 
-""" Define file names """
-fname_interpolated = '53_SBack_2_1164_dil20_edited.csv'  # nuclei detection csv file
-fname_translation = 'Translation1.csv'  # matlab translation csv file from nuclei detection
+def make_covariance(data):
+    """
+    Constructs a covariance matrix using S values
 
-""" Get centroids """
-centroid_interpolated = get_centroid(os.path.join(root_dir, fname_interpolated), cols=[9,12])  # change cols if cx,cy,cz columns change
-centroid_translation = get_centroid(os.path.join(root_dir, fname_translation))
+    :param data: np array of shape 1x6
+    :return:
+    """
 
-""" Get interpolation """
-xy_interpolated = intepolate(centroid_interpolated, centroid_translation)
+    return np.array(
+        [
+            [data[0], data[1], data[2]],
+            [data[1], data[3], data[4]],
+            [data[2], data[4], data[5]]
+        ]
+    )
 
-""" Translate back """
-centroid_translated_back = translate(centroid_interpolated, xy_interpolated)
 
-""""""
-save_file(os.path.join(root_dir, fname_interpolated), centroid_translated_back, header=True, cols=[9,12])   # change cols if cx,cy,cz columns change
+def svd(M):
+    """
+    Performs singular value decomposition on a 3x3 covariance matrix
+
+    :param M: np array of shape 3x3
+    :return:
+    """
+
+    U, s, V = np.linalg.svd(M)
+
+    return U, s, V
